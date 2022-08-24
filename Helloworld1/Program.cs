28624853434a12fbebd863c2f463590bf13fbe0e
darcaos <darcaos@DESKTOP-8MB4QNC>
2022-08-24 13:29:19 -0300

    Add project files.

---
+++ Helloworld1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+using System;
+using System.Net.NetworkInformation;
+
+namespace Miprograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperatura = 150;
+            string estado;
+            estado = temperatura < 0 ? "Solido" : temperatura > 99 ? "Gaseoso" : "Liquido";
+            Console.WriteLine("El estado del agua es {0}", estado);
+            Console.Read();
+        }
+    } 
+}
